0689b3a85a129ce1cf35717b4cfba0889853b216
Asumu Takikawa <asumu@ccs.neu.edu>
2014-01-31 00:09:14 -0500

    Add dependencies to package metadata

---
+++ info.rkt
@@ -0,0 +1,7 @@
+#lang info
+
+(define deps '("base" "ragg" "draw-lib" "math-lib"
+               "parser-tools-lib" "pict-lib" "slideshow-lib"
+               "srfi-lite-lib" "unstable-lib"))
+(define build-deps '("at-exp-lib" "rackunit-lib" "scribble-lib"))
+
