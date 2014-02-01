374f0b986b5b449ca0799b1d15622a6ddfa6f362
Jens Axel Søgaard <jensaxel@soegaard.net>
2014-02-01 16:33:21 +0100

    Added first-value

--- metapict/def.rkt
+++ metapict/def.rkt
@@ -11,3 +11,13 @@
 (defm (list -π -3π/4 -π/2 -π/4 _ π/4 π/2 3π/4 π)      (for/list ([n (in-range -1 5/4 1/4)]) (* pi n)))
 (defm (list 2pi -2pi) (list (* 2 pi) (* -2 pi)))
 (defv (2π -2π) (values 2pi -2pi))
+
+(define-syntax (first-value stx)
+  (syntax-case stx ()
+    [(_ expr ...)
+     (syntax/loc stx
+       (call-with-values (λ() expr ...) 
+                         (λ xs (car xs))))]))
+
+(module+ test (require rackunit)
+  (check-equal? (first-value (values 1 2)) 1))
