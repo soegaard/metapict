3be1f54a482907f6555045a6afc3de09b40f07e9
Asumu Takikawa <asumu@ccs.neu.edu>
2014-02-07 13:21:34 -0500

    Add missing build-dep in info.rkt

--- info.rkt
+++ info.rkt
@@ -3,5 +3,6 @@
 (define deps '("base" "draw-lib" "math-lib"
                "parser-tools-lib" "pict-lib" "slideshow-lib"
                "srfi-lite-lib" "unstable-lib"))
-(define build-deps '("at-exp-lib" "rackunit-lib" "scribble-lib"))
+(define build-deps '("at-exp-lib" "rackunit-lib" "scribble-lib"
+                     "racket-doc"))
 
