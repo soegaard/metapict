eb6893a70c02104585006941e1b0c49f6ded53b6
Jens Axel Søgaard <jensaxel@soegaard.net>
2014-02-08 19:09:55 +0100

    Made metapict a multi collection package

--- info.rkt
+++ info.rkt
@@ -1,8 +1,11 @@
 #lang info
-
+(define collection 'multi)
+(define version    "0.1")
 (define deps '("base" "draw-lib" "math-lib"
                "parser-tools-lib" "pict-lib" "slideshow-lib"
                "srfi-lite-lib" "unstable-lib"))
+
+
 (define build-deps '("at-exp-lib" "rackunit-lib" "scribble-lib"
                      "racket-doc"))
 
--- metapict/metapost-examples/example2.rkt
+++
@@ -1,5 +0,0 @@
-beginfig(1)
-  path p;
-  p = (0,0){curl 1} .. tension 2 and 3 ..(1,1) ..{down}(2,0) -- (3,0);
-  show p;
-endfig;
