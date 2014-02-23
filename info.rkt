9652c5fb86f90bbea9beb54e4e871a61c0d2d565
Asumu Takikawa <asumu@ccs.neu.edu>
2014-02-23 14:15:13 -0500

    Update package dependencies

--- info.rkt
+++ info.rkt
@@ -1,11 +1,11 @@
 #lang info
 (define collection 'multi)
 (define version    "0.1")
-(define deps '("base" "draw-lib" "math-lib"
+(define deps '("base" "draw-lib" "math-lib" "gui-lib"
                "parser-tools-lib" "pict-lib" "slideshow-lib"
-               "srfi-lite-lib" "unstable-lib"))
+               "srfi-lite-lib" "unstable-lib" "htdp-lib"))
 
 
 (define build-deps '("at-exp-lib" "rackunit-lib" "scribble-lib"
-                     "racket-doc"))
+                     "racket-doc" "draw-doc" "pict-doc"))
 
