4dfbab6f5055e5963ab8bf980248dc1acbfc2f91
Jens Axel Søgaard <jensaxel@soegaard.net>
2014-03-23 21:07:00 +0100

    Forgot to rename det to vec-det in the tests

--- metapict/pt-vec.rkt
+++ metapict/pt-vec.rkt
@@ -105,8 +105,8 @@
   (check-equal? (len (vec 1 1)) (sqrt 2))
   (check-equal? (rot90 east) north)
   (check-equal? (rot90 north) west)
-  (check-equal? (det (vec 1 0) (vec 2 0)) 0)
-  (check-equal? (det (vec 1 0) (vec 0 2)) 2)
+  (check-equal? (vec-det (vec 1 0) (vec 2 0)) 0)
+  (check-equal? (vec-det (vec 1 0) (vec 0 2)) 2)
   
   )
 (module+ test
