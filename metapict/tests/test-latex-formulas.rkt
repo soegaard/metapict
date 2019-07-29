fd20de53e5dc71f79a4198f7b16077451a3cc8a0
Jens Axel Søgaard <jensaxel@soegaard.net>
2019-07-29 16:04:19 +0200

    Disable test with poppler

---
+++ metapict/tests/test-latex-formulas.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+;(require racket-poppler racket-poppler/render-tex)
+
+;(latex-path "/usr/local/texlive/2013/bin/universal-darwin/pdflatex")
+
+;(latex-path "pdflatex")
+;(latex->bitmap "$\\sqrt{x^2+y^2}$")
